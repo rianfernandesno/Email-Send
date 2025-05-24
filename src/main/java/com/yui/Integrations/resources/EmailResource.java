49416c34ef67299ac82cb219f5bdb972ad74e3b3
rianfernandesno <zrpapz12@gmail.com>
2025-05-24 13:33:33 -0300

    EmailService

--- src/main/java/com/yui/Integrations/resources/EmailResource.java
+++ src/main/java/com/yui/Integrations/resources/EmailResource.java
@@ -1,6 +1,8 @@
 package com.yui.Integrations.resources;
 
 import com.yui.Integrations.dto.EmailDTO;
+import com.yui.Integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+    @Autowired
+    private EmailService service;
+
     @PostMapping
     public ResponseEntity<Void> sendEmail(@RequestBody EmailDTO emailDTO){
-
+        service.sendEmail(emailDTO);
         return ResponseEntity.noContent().build();
     }
 }
